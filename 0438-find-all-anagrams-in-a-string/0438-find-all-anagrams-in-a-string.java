6698660e0dd2f3af847883f7fb315b8613344dfa
R.MEGHANADH <githubfor169@gmail.com>
2026-06-28 13:19:21 +0530

    Time: 46 ms (27.13%), Space: 47.5 MB (24.21%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,23 +1,35 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        ArrayList<Integer> list =new ArrayList<>();
-        int m=s.length();
-        int n=p.length();
-        int[] parr=new int[26];
+        ArrayList<Integer> list=new ArrayList<>();
+        HashMap<Character,Integer> pmap=new HashMap<>();
         for(char ch:p.toCharArray()){
-            parr[ch-'a']++;
+            pmap.put(ch,pmap.getOrDefault(ch,0)+1);
         }
-       for(int i=0;i<=m-n;i++){
-        int[] sarr=new int[26];
-        for(int j=i;j<i+n;j++){
-            sarr[s.charAt(j)-'a']++;
-        }
-        if(Arrays.equals(parr,sarr)){
-            list.add(i);
-        }
-       }
-       return list;
+        HashMap<Character,Integer>smap=new HashMap<>();
+        int count=p.length();
+        int l=0;
+        for(int r=0;r<s.length();r++){
+            char c=s.charAt(r);
+            smap.put(c,smap.getOrDefault(c,0)+1);
+            if(pmap.containsKey(c)&&smap.get(c)<=pmap.get(c)){   //expansion phase
+                count--;
 
-        
+            } 
+            if(r-l+1>p.length()){    //shrinking phase
+               char c2=s.charAt(l);
+               if(pmap.containsKey(c2)&&smap.get(c2)<=pmap.get(c2)){
+                count++;
+
+               }
+               smap.put(c2,smap.get(c2)-1);
+               l++;
+            
+            }
+            if(count==0){
+                list.add(l);
+
+            }
+        }
+        return list;
     }
 }

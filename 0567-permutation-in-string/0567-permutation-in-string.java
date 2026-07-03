ce8661b69e3bfa4fce6d879531f8d33a910112a8
R.MEGHANADH <githubfor169@gmail.com>
2026-07-03 11:37:03 +0530

    Time: 19 ms (31.92%), Space: 44.5 MB (32.3%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,29 +1,29 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-         int m=s1.length();
-         int n=s2.length();
-         int[] freq1=new int[26];
-         int[] freq2=new int[26];
-         int l=0;
-         for(char ch:s1.toCharArray()){
-            freq1[ch-'a']++;
-         }
-         for(int r=0;r<n;r++){
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s1.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int count=s1.length();
+        int l=0;
+        for(int r=0;r<s2.length();r++){
             char c=s2.charAt(r);
-            
-            freq2[c-'a']++;
-            if(r-l+1>m){
-              freq2[s2.charAt(l)-'a']--;
-              l++;
-
-            }
-            
-            if(Arrays.equals(freq1,freq2)){
+           int val=map.getOrDefault(c,0);
+            if(val>0)count--;
+            map.put(c,val-1);
+            if(count==0){
                 return true;
             }
+            if(r-l+1==s1.length()){
+                char leftc=s2.charAt(l);
+                int leftval=map.get(leftc);
+                if(leftval>=0)count++;
+                map.put(leftc,leftval+1);
+                l++;
 
-
-         }
-         return false;
+            }
+        }
+        return false;
+        
     }
 }

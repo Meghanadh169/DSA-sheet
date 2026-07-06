974800347f4842d48981a00eaca504c05310cb14
R.MEGHANADH <githubfor169@gmail.com>
2026-07-06 16:40:22 +0530

    Time: 1 ms (97.66%), Space: 42.5 MB (78.16%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+         if(x<2) return x;
+         long l=1;
+         long r=x/2;
+         while(l<=r){
+            long mid=l+(r-l)/2;
+            long sqr=mid*mid;
+            if(sqr==x)return (int) mid;
+            if(sqr>x) r=mid-1;
+            else{
+                l=mid+1;
+            }
+
+         }
+         return (int) r;
+
+
+    }
+}
